57358b34eefa00def6d9f3cccea8cd46b6a1dbf0
felippefardin <felippefardin@gmail.com>
2025-03-23 13:08:00 -0300

    Adicional do modal de contato enviar mensagem

--- email.php
+++ email.php
@@ -1,3 +1,34 @@
+<?php
+
+
+if (!empty($_POST['nome']) && !empty($_POST['email']) && !empty($_POST['telefone']) && !empty($_POST['comentario'])) {
+    $to = "felippefardin@hotmail.com";
+    $nome = trim($_POST['nome']);
+    $email = trim($_POST['email']);
+    $telefone = trim($_POST['telefone']);
+    $comentario = trim($_POST['comentario']);
+    $assunto = "Mensagem via site";
+
+    $mailHeaders = "From: " . $email . "\r\n" . 
+                   "Reply-To: " . $email . "\r\n" .
+                   "MIME-Version: 1.0\r\n" .
+                   "Content-Type: text/plain; charset=UTF-8\r\n" .
+                   "Nome: " . $nome . "\r\n" . 
+                   "Email: " . $email . "\r\n" . 
+                   "Telefone: " . $telefone . "\r\n" . 
+                   "Comentário: " . $comentario . "\r\n";  
+
+    if (mail($to, $assunto, $mailHeaders)) {
+        header("Location: email.php");
+        exit;
+    } else {
+        echo "Erro ao enviar o e-mail.";
+    }
+} else {
+    echo "Preencha todos os campos obrigatórios.";
+}
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -7,7 +38,7 @@
     <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
-    <div class="m-conteudo" id="modal-sucess">
+    <div class="m-conteudo" id="modal-sucesso">
 
         <div class="modal">
             <img src="img/logo.png" alt="logotipo mercado">
@@ -19,5 +50,7 @@
         </div>
 
     </div>
+
+    <script src="js/sucesso.js"></script>
 </body>
 </html>
